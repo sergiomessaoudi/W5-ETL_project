99ad902a1f9301612033a62d77eac6ba38a69f80
“Fominayasg” <fominayasg@gmail.com>
2022-04-15 10:35:19 +0200

    ready:whale:

--- src/apifunctions.py
+++ src/apifunctions.py
@@ -1,18 +1,17 @@
-'''En este archivo pondré todas mis funciones de extracción
-de datos de la API.'''
+#En este archivo pondré todas mis funciones de extracción de datos de la API.
 
 # Recuerda: Todas las funciones deben tener un docstring(documentación entre triples comillas que nos cuenta lo que hace la función)
 
 #Importo las librerías necesarias para las funciones del archivo
 
-import hacercosas
+#import hacercosas
 
 
 
 #Escribo mis funciones
-def datitos_a_mi (atr1,atr2):
-    '''Esta función recibe dos atributos hace cosas'''
-    return atr1.hacercosas(atr2)
+#def datitos_a_mi (atr1,atr2):
+    #'''Esta función recibe dos atributos hace cosas'''
+    #return atr1.hacercosas(atr2)
 
 def suma_dos_numeros(num1,num2):
     '''Esta función recibe dos números y devuelve la suma de ambos'''
